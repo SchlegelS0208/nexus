19bca54376ca9cef729d929adb7a071e47d7940d
Steven Schlegel <sschlegel@eos-uptrade.de>
2017-10-28 14:13:36 +0200

    fix nexus file-descriptors and add puppet default.pp for vagrant-based environments to github repo

---
+++ puppet/manifests/default.pp
@@ -0,0 +1,34 @@
+### NEXUS Server Installation -- for Nexus 3.x.x ONLY!!!
+node default {
+  notify { "Preparing installation of Nexus OSS": }
+
+  if $::operatingsystem == 'Debian' or $::operatingsystem == 'Ubuntu' {
+    notify { "Found Debian-based OS: $::operatingsystem": }
+
+    class { 'jdk_oracle':
+      jce            => true,
+      version_update => '151',
+      version_build  => '12',
+      version_hash   => 'e758a0de34e24606bca991d704f6dcbf',
+      default_java   => true,
+      before         => Class['nexus'],
+    }
+  } else {
+    notify { "Found RPM-based OS: $::operatingsystem": }
+
+    class { 'java':
+      before         => Class['nexus'],
+    }
+  }
+
+  notify { "Running installation procedure of Nexus OSS": }
+  class { 'nexus':
+    version               => '3.6.0',
+    revision              => '02',
+    download_site         => 'https://sonatype-download.global.ssl.fastly.net/nexus/3',
+    nexus_type            => 'unix',
+    nexus_work_dir_manage => false,
+    nexus_root            => '/opt',
+    download_folder       => '/opt',
+  }
+}
